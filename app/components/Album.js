0bb4dde232cec522504c01d199fc6576d81ac542
sirgeorge <gsmulbah2500@gmail.com>
2022-11-15 00:06:26 +0000

    completed the homeScreen UI

--- app/components/Album.js
+++ app/components/Album.js
@@ -5,11 +5,13 @@ import colors from '../config/colors'
 
 
  
- function Album() {
+ function Album({albumData}) {
+ 
+ 
   return (
     <View  style={styles.container}>
-      {/* <Image source={{uri:}} style={styles.img} /> */}
-      <Text style={styles.text}>Album</Text>
+       <Image source={{uri:albumData.imageUri}} style={styles.img} /> 
+      <Text style={styles.text}>{albumData.artistsHeadline}</Text>
     </View>
   )
 }
@@ -19,13 +21,16 @@ export default Album;
 
 const styles = StyleSheet.create({
  container:{
-  
+  width:155,
+  margin:10,
  },
  img:{
-
+ width:'100%',
+ height:155,
  },
 
  text:{
-  color:colors.light
+  color:colors.light,
+  marginTop:5,
 }
 })
---
+++ app/components/AlbumCategory.js
@@ -0,0 +1,34 @@
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+import React from 'react'
+import colors from '../config/colors'
+import Album from './Album'
+
+
+
+export default function AlbumCategory({data}) {
+  return (
+    <View  style={styles.container}> 
+      <Text style={styles.title}>{data.title}</Text>
+      <FlatList
+       data={data.albums}
+       renderItem={({item}) => <Album albumData={item}/>}
+       keyExtractor={(item) => item.id }
+       showsHorizontalScrollIndicator={false}
+       horizontal
+      />    
+    </View>
+  )
+}    
+
+
+const styles = StyleSheet.create({
+  container:{
+   margin:10,
+  },
+  title:{
+    color:colors.light,
+    margin:10,
+    fontSize:18,
+    fontWeight:'bold'
+  }
+})
