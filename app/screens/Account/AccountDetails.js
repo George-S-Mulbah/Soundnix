f54e87368ad7289271ec7fa81a774a8914901855
sirgeorge <gsmulbah2500@gmail.com>
2022-11-27 23:00:38 +0000

    Account Screen Header Created successfully

--- app/screens/Account/AccountDetails.js
+++ app/screens/Account/AccountDetails.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View , ImageBackground ,Image, TouchableOpacity} from
 import React from 'react'
 import Screen from '../../components/Screen'
 import colors from '../../config/colors'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Entypo } from '@expo/vector-icons';
 
 
@@ -18,12 +19,40 @@ export default function AccountDetails() {
          <Entypo name="camera" size={24} color="black" />
          </TouchableOpacity>
         </View>
-        <View style={styles.profileImage}>
+      </ImageBackground>
+      <View style={styles.profileImageContainer}>
+      <View style={styles.profileImage}>
+      <View  style={styles.BorderProfile}>
             <Image source={{uri:'https://images.unsplash.com/photo-1535146851324-6571dc3f2672?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fFIlMjZifGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60'}} 
              style={{width:'100%' , height:'100%', borderRadius:50}} 
              />
         </View>
-      </ImageBackground>
+      </View>
+         <View  style={styles.userDetails}>
+        <Text   style={styles.userName}>George S Mulbah</Text>
+         <View style={styles.verify}>
+         <MaterialCommunityIcons
+                  color={colors.brandColor}
+                  name="checkbox-marked-circle"
+                  size={20}
+                />
+         </View>
+         </View>
+        <View  style={styles.userFollower_followingContainer}>
+         <View  style={styles.userFollower}>
+        <Text   style={styles.userNumberOffollowers}>2M</Text>
+        <Text   style={styles.userNumberOffollowersText}>followers</Text>
+        </View>
+        <View>
+            <Text style={styles.userFollowerDot}>.</Text>
+        </View>
+        <View  style={styles.userFollower}>
+        <Text   style={styles.userNumberOffollowersText}>following</Text>
+        <Text   style={styles.userNumberOffollowing}>0</Text>
+        </View>
+       
+        </View>
+      </View> 
       
      </View>
     </Screen>
@@ -37,16 +66,79 @@ container:{
     backgroundColor:"red",
 },
 
-profileImage:{
-height:100,
-width:100,
-marginTop:45,
-marginLeft:20,
-backgroundColor:"blue",
-borderRadius:50,
-borderWidth:2,
-borderColor: colors.brandColor
+userNumberOffollowing:{
+    color:colors.brandColor,
+    fontWeight:"bold",
+    marginLeft: 10,
+    marginTop:1.2,
 },
+
+userFollowerDot:{
+color:colors.brandColor,
+marginLeft:10,
+marginRight:5,
+fontSize:20,
+fontWeight:"bold",
+marginTop:-9,
+
+},
+userFollower_followingContainer:{
+ marginTop:10,
+ flexDirection:"row",
+},
+
+verify:{
+    marginTop:5,
+},
+userNumberOffollowersText:{
+    marginLeft:5,
+    color:colors.light
+},
+userNumberOffollowers:{
+
+color:colors.brandColor,
+fontWeight:"bold",
+marginLeft: 10,
+},
+userFollower:{
+flexDirection:"row"
+},
+userDetails:{
+marginTop:10,
+marginLeft:10,
+flexDirection:"row"
+},
+
+
+userName:{
+color:colors.light,
+fontSize:20,
+fontWeight:"bold"
+}
+,
+profileImage:{
+height:115,
+width:115,
+marginTop:-60,
+marginLeft:20,
+backgroundColor:colors.black,
+borderRadius:0.5*115,
+borderWidth:2.5,
+alignItems:"center",
+justifyContent:"center",
+borderColor: colors.black
+},
+BorderProfile:{
+    height:100,
+    width:100,
+    backgroundColor:"red",
+    borderColor: colors.brandColor,
+    borderRadius:0.5*100,
+    borderWidth:1,
+   
+   
+}
+,
 
 uploadIconContainer:{
     backgroundColor:colors.medium,
@@ -61,6 +153,11 @@ uploadIconContainer:{
     marginLeft:230,
 
 
+},
+
+profileImageContainer:{
+    backgroundColor:colors.black,
+    height:160,
 }
     
 })
